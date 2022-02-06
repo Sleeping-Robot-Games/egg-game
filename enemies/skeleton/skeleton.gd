d8b10a6ef600c489a1a3f1ade428bdf00397cd60
Ross <ross@eml.cc>
2022-02-06 15:31:52 -0800

    Improve enemy animation logic and enemy spawner positioning logic

--- enemies/skeleton/skeleton.gd
+++ enemies/skeleton/skeleton.gd
@@ -7,26 +7,50 @@ export (String, 'Left', 'Right', 'Front', 'Back') var default_facing = 'Front'
 onready var anim_player = $AnimationPlayer
 onready var exclamation = $Exclamation
 
+var despawn_if_offscreen = false
 var velocity = Vector2.ZERO
+var destination = Vector2.ZERO
+var node_target = null
 var player: KinematicBody2D = null
 var facing = "Right"
 
-#func animate_sprite(from, to):
-#	# Determine which way to face enemy based on prior position
-#	var dir = from.direction_to(to)
-#	var dominant_axis = "x" if abs(dir.x) > abs(dir.y) else "y"
-#	var new_facing = facing
-#	if dominant_axis == "x":
-#		new_facing = "Right" if dir.x > 0 else "Left"
-#	else:
-#		new_facing = "Front" if dir.y > 0 else "Back"
-#
-#	anim_player.play("Walk" + new_facing)
-#		
-#	# Animate enemy in appropriate direction
-#	if facing != new_facing:
-#		facing = new_facing
-#		anim_player.play("Walk" + facing)
+
+func _physics_process(delta):
+	# only despawn if off screen after enemy has first appeared on screen
+	if not despawn_if_offscreen and get_node("VisibilityNotifier2D").is_on_screen():
+		despawn_if_offscreen = true
+	elif despawn_if_offscreen and not get_node("VisibilityNotifier2D").is_on_screen():
+		queue_free()
+	
+	# move towards target
+	var target = destination
+	if node_target:
+		target = node_target.global_position
+	velocity = global_position.direction_to(target) * move_speed
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		pass
+	
+	# animate sprite based on movement direction
+	animate_sprite(global_position, target)
+
+
+func animate_sprite(from, to):
+	# determine which way to face enemy based on prior position
+	var dir = from.direction_to(to)
+	var dominant_axis = "x" if abs(dir.x) > abs(dir.y) else "y"
+	var new_facing = facing
+	if dominant_axis == "x":
+		new_facing = "Right" if dir.x > 0 else "Left"
+	else:
+		new_facing = "Front" if dir.y > 0 else "Back"
+
+	anim_player.play("Walk" + new_facing)
+		
+	# animate enemy in appropriate direction
+	if facing != new_facing:
+		facing = new_facing
+		anim_player.play("Walk" + facing)
 
 
 #func chase_check():
--- spawners/enemy_spawner/enemy_spawner.gd
+++ spawners/enemy_spawner/enemy_spawner.gd
@@ -1,21 +1,74 @@
 extends Node2D
 
 onready var game = get_parent().get_parent()
-onready var viewport_width = get_viewport().size.x
 var skeleton = preload("res://enemies/skeleton/skeleton.tscn")
 
-func _ready():
-	print("difficulty: " + str(game.difficulty))
-	print("screen_size: " + str(viewport_width))
-
+func spawn_enemy(enemy_scene):
+	# determine spawn position and destination position
+	var viewport = get_viewport()
+	var half_viewport_width = viewport.size.x / 2
+	var half_viewport_height = viewport.size.y / 2
+	var spawn_pos = Vector2.ZERO
+	var destination_pos = Vector2.ZERO
+	var rng = RandomNumberGenerator.new()
+	# spawn_loc indicates if spawning to the left, top, or right of viewport
+	rng.randomize()
+	var spawn_loc = rng.randi_range(0, 2)
+	print(spawn_loc)
+	if spawn_loc == 0:
+		# spawn will be left of viewport and top half of screen
+		rng.randomize()
+		var spawn_x = (half_viewport_width * -1) -32
+		var spawn_y = rng.randi_range(0, half_viewport_height)
+		spawn_pos = Vector2(spawn_x, spawn_y)
+		# destination will be right of viewport and bottom half of screen
+		rng.randomize()
+		var destination_x = half_viewport_width + 32
+		var destination_y = rng.randi_range(half_viewport_height, viewport.size.y)
+		destination_pos = Vector2(destination_x, destination_y)
+	elif spawn_loc == 1:
+		# spawn will be above viewport
+		rng.randomize()
+		var spawn_x = rng.randi_range(half_viewport_width * -1, half_viewport_width)
+		var spawn_y = -32
+		spawn_pos = Vector2(spawn_x, spawn_y)
+		# destination will be below viewport
+		rng.randomize()
+		var destination_x = rng.randi_range(0, viewport.size.x)
+		var destination_y = viewport.size.y + 32
+		destination_pos = Vector2(destination_x, destination_y)
+	elif spawn_loc == 2:
+		# spawn will be right of viewport and top half of screen
+		rng.randomize()
+		var spawn_x = half_viewport_width + 32
+		var spawn_y = rng.randi_range(0, half_viewport_height)
+		spawn_pos = Vector2(spawn_x, spawn_y)
+		# destination will be left of viewport and bottom half of screen
+		rng.randomize()
+		var destination_x = (half_viewport_width * -1) -32
+		var destination_y = rng.randi_range(half_viewport_height, viewport.size.y)
+		destination_pos = Vector2(destination_x, destination_y)
+	
+	# spawn and initialize enemy
+	var enemy_instance = enemy_scene.instance()
+	enemy_instance.global_position = spawn_pos
+	enemy_instance.destination = destination_pos
+	add_child(enemy_instance)
+	
 
 func _on_SpawnTimer_timeout():
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
+	# todo: factor in player count and make spawn logic more dynamic
 	if game.difficulty == 0:
-		randomize()
-		var x = rng.randi_range(0, viewport_width)
-		print("x: " + str(x))
-		var skeleton_instance = skeleton.instance()
-		skeleton_instance.global_position = Vector2(x, 0)
-		add_child(skeleton_instance)
+		spawn_enemy(skeleton)
+	elif game.difficulty == 1:
+		spawn_enemy(skeleton)
+		spawn_enemy(skeleton)
+	elif game.difficulty == 2:
+		spawn_enemy(skeleton)
+		spawn_enemy(skeleton)
+		spawn_enemy(skeleton)
+	elif game.difficulty >= 3:
+		spawn_enemy(skeleton)
+		spawn_enemy(skeleton)
+		spawn_enemy(skeleton)
+		spawn_enemy(skeleton)
