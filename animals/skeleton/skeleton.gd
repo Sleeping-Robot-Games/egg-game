ab1b08ff532a055cc495860618d531387e9e9ab4
Ross <ross@eml.cc>
2022-02-22 14:29:10 -0800

    Change enemy movement to stop on collisions
    Fixes bug where enemies could push player past map boundaries

--- animals/skeleton/skeleton.gd
+++ animals/skeleton/skeleton.gd
@@ -31,11 +31,10 @@ func _physics_process(delta):
 	var target_pos = destination
 	if node_target:
 		target_pos = node_target.global_position
-	global_position = global_position.move_toward(target_pos, delta * move_speed)
-	#velocity = global_position.direction_to(target_pos) * move_speed
-	#var collision = move_and_slide(velocity)
-	#if collision:
-		#pass
+	velocity = global_position.direction_to(target_pos) * move_speed
+	var collision = move_and_slide(velocity)
+	if collision:
+		pass
 	
 	# animate sprite based on movement direction
 	animate_sprite(global_position, target_pos)
